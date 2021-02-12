a2029a921d692f32152fc617646eb06a0e212990
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-12 08:20:43 +0000

    UI setup

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# App Constants
+WORK = 25
+SHORT_BREAK = 5
+LONG_BREAK = 20
+
+# App Interface
+interface = Tk()
+interface.title("A3AJAGBE POMODORO APP")
+interface.minsize(width=500, height=300)
+
+interface.mainloop()
