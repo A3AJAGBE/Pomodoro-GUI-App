f72baf4385f3ecf94684a61ac8f8445f3a3bb9b5
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-12 09:16:42 +0000

    Update commit

--- main.py
+++ main.py
@@ -5,26 +5,27 @@ WORK = 25
 SHORT_BREAK = 5
 LONG_BREAK = 20
 BG_COLOR = "#222831"
+FONT = "Courier"
 
 # App Interface
 interface = Tk()
 interface.title("A3AJAGBE POMODORO APP")
 interface.config(padx=100, pady=50, bg=BG_COLOR)
 
-timer = Label(text="Timer", fg="#f05454", bg=BG_COLOR, font=("Courier", 60, "bold"))
+timer = Label(text="Timer", fg="#f05454", bg=BG_COLOR, font=(FONT, 60, "bold"))
 timer.grid(column=1, row=0)
 
 # Add background image using canvas widget
 canvas = Canvas(width=160, height=130, bg=BG_COLOR, highlightthickness=0)
 bg_image = PhotoImage(file="pomodoro_image.png")
 canvas.create_image(80, 65, image=bg_image)
-canvas.create_text(80, 75, text="00:00", fill="white", font=("Courier", 40, "bold"))
+canvas.create_text(80, 75, text="00:00", fill="white", font=(FONT, 40, "bold"))
 canvas.grid(column=1, row=1)
 
-start = Button(text="Start")
+start = Button(text="Start", highlightthickness=0)
 start.grid(column=0, row=2)
 
-reset = Button(text="Reset")
+reset = Button(text="Reset", highlightthickness=0)
 reset.grid(column=2, row=2)
 
 check = Label(text="✅", bg=BG_COLOR)
