de3d7ff65f16b31d39100139f4f9ac2e0a38cc4f
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-12 09:11:37 +0000

    Complete UI setup

--- main.py
+++ main.py
@@ -11,12 +11,24 @@ interface = Tk()
 interface.title("A3AJAGBE POMODORO APP")
 interface.config(padx=100, pady=50, bg=BG_COLOR)
 
+timer = Label(text="Timer", fg="#f05454", bg=BG_COLOR, font=("Courier", 60, "bold"))
+timer.grid(column=1, row=0)
+
 # Add background image using canvas widget
 canvas = Canvas(width=160, height=130, bg=BG_COLOR, highlightthickness=0)
 bg_image = PhotoImage(file="pomodoro_image.png")
 canvas.create_image(80, 65, image=bg_image)
-canvas.create_text(80, 75, text="00:00", fill="white", font=("Arial", 40, "bold"))
-canvas.pack()
+canvas.create_text(80, 75, text="00:00", fill="white", font=("Courier", 40, "bold"))
+canvas.grid(column=1, row=1)
+
+start = Button(text="Start")
+start.grid(column=0, row=2)
+
+reset = Button(text="Reset")
+reset.grid(column=2, row=2)
+
+check = Label(text="✅", bg=BG_COLOR)
+check.grid(column=1, row=3)
 
 # Keep the app open until exited
 interface.mainloop()
