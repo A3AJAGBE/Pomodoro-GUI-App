52b51159fb15607833d2f255cf0e16250bcd8350
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-12 09:42:26 +0000

    Add start timer function

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from tkinter import *
+import math
 
 
 def count_down(count):
     """This function counts down the timer"""
-    canvas.itemconfig(timer_text, text=count)
+    # Set up the time display
+    minutes = math.floor(count / 60)
+    seconds = count % 60
+
+    if minutes < 10:
+        minutes = f"0{minutes}"
+
+    if seconds < 10:
+        seconds = f"0{seconds}"
+
+    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
     if count > 0:
         interface.after(1000, count_down, count - 1)
+
+
+def start_timer():
+    """This function calls the countdown function"""
+    # multiply the count by 60
+    count_down(1 * 60)
 
 
 # App Constants
@@ -30,10 +47,7 @@ canvas.create_image(80, 65, image=bg_image)
 timer_text = canvas.create_text(80, 75, text="00:00", fill="white", font=(FONT, 40, "bold"))
 canvas.grid(column=1, row=1)
 
-# Add the count down function
-count_down(10)
-
-start = Button(text="Start", highlightthickness=0)
+start = Button(text="Start", highlightthickness=0, command=start_timer)
 start.grid(column=0, row=2)
 
 reset = Button(text="Reset", highlightthickness=0)
